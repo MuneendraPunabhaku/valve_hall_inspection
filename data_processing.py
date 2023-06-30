dac9a0961927f2c51b8e2db2906e6160d0ac2707
slybeetle18 <mpunabhaku@icloud.com>
2023-06-30 15:18:43 +0530

    addded factorial code

--- data_processing.py
+++ data_processing.py
@@ -3,4 +3,10 @@ print("adding all the data processing files to this file")
 
 def pascal_traiangle(n):
     for i in range(n):
-        pascal_number=n_factorial/i_factorial8(n-i-1)_factorial
+        pascal_number=n_factorial/i_factorial8(n-i-1)i_factorial8
+
+def facto(n):
+    if n==0:
+        return 1
+    else:
+        return n*facto(n-1)
